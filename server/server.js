27bebc83b689c125da2241995d1837766b0c323e
Edgardo Beltran <ebeltran@mscgroupny.com>
2018-06-29 11:26:18 -0400

    setup prod build

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const path = require('path');
 const express = require('express');
-const app = express();
+const app = express(); // express application
 const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //for heroku - dynamic port
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // use public directory 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.join(publicPath, 'index.html')); // if page not in public render this
 });
 
 app.listen(port, () => {
-  console.log('Server is up!');
+  console.log('Server is up!'); // start up here
 });
